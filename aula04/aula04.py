99cbd2c78e019d42e657c2c7e9262d9b340a2c14
Caio Eduardo de Albuquerque Magalhães <69921724+ravencaio@users.noreply.github.com>
2025-08-14 22:02:29 +0000

    aula 04

---
+++ aula04/aula04.py
@@ -0,0 +1,12 @@
+fibonacci = [0,1]
+quant = int(input('Quantos digitos de fibonacci você quer ver: '))
+match quant:
+    case 0:
+        exit
+    case 1:
+        print(0)
+    case _:
+        for c in range(quant-2):
+            novo = fibonacci[-1] + fibonacci[-2]
+            fibonacci.append(novo)
+        print(fibonacci)
