af2cbe124aa702d190aedcd763f2202099c43275
Caio Eduardo de Albuquerque Magalhães <69921724+ravencaio@users.noreply.github.com>
2025-09-02 22:02:33 +0000

    foda

--- lista-exercicios/ex11.py
+++ lista-exercicios/ex11.py
@@ -1,16 +1,15 @@
 cas_vaz = [['   '] for c in range(8)]
-inicio_pretas = [['tor'],['cav'],['bis'],['rai'],['rei'],['bis'],['cav'],['tor']]
+inicio_peças = [['tor'],['cav'],['bis'],['rai'],['rei'],['bis'],['cav'],['tor']]
 inicio_peoes = [['pea'] for c in range(8)]
-inicio_brancas = inicio_pretas.copy()[::-1]
 
 for c in range(6):
     if c == 0:    
-        print(inicio_pretas)
+        print(inicio_peças)
         print(inicio_peoes)
         continue
     if c == 5:
         print(inicio_peoes)
-        print(inicio_brancas)
+        print(inicio_peças[::-1])
         break
     print(cas_vaz)
     
