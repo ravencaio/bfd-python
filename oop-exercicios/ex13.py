aeae1c3e5bb674383b88684b0e99dd13533317de
Caio Eduardo de Albuquerque Magalhães <69921724+ravencaio@users.noreply.github.com>
2025-09-16 23:56:17 +0000

    exercicios 16/09

--- oop-exercicios/ex13.py
+++ oop-exercicios/ex13.py
@@ -1,16 +1,17 @@
 class Computador:
     departamento = 'Comum'
-    def __init__(self, modelo, processador, memoria):
-        self.modelo = modelo
+    modelo = 'Dell Inspiron 15'
+    def __init__(self, processador, memoria):
         self.processador = processador
         self.memoria = memoria
 
 class CompIA(Computador):
     departamento = 'IA'
-    def __init__(self, modelo, processador, memoria, placa_de_video):
-        super().__init__(modelo, processador, memoria)
+    modelo = 'Dell G15'
+    def __init__(self, processador, memoria, placa_de_video):
+        super().__init__(processador, memoria)
         self.placa_de_video = placa_de_video
 
-c1 = Computador('Dell Inspiron 15', 'I5-12250U', '8 GB')
-c2 = CompIA('Dell G15', 'I9-12420', '32 GB', 'RTX 4060Ti')    
+c1 = Computador('I5-12250U', '8 GB')
+c2 = CompIA('I9-12420', '32 GB', 'RTX 4060Ti')    
 
